d355f24023af584f33db95699296841e2aaae131
David Maciel Pereira <david@pop-os.localdomain>
2021-01-30 21:00:07 -0300

    feat: middleware of authentication

---
+++ migrations/1612040580894-users_companies.ts
@@ -0,0 +1,54 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class usersCompanies1612040580894 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'user_company',
+        columns: [
+          {
+            name: 'user_id',
+            type: 'int',
+          },
+          {
+            name: 'company_id',
+            type: 'int',
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'user_company',
+      new TableForeignKey({
+        name: 'user_company',
+        referencedTableName: 'users',
+        columnNames: ['user_id'],
+        referencedColumnNames: ['id'],
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'user_company',
+      new TableForeignKey({
+        name: 'company_user',
+        referencedTableName: 'companies',
+        columnNames: ['company_id'],
+        referencedColumnNames: ['id'],
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('user_company');
+  }
+}
