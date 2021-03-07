63a7034fd150dde0dadc1e8100c3ecb5c306bc21
kow <kaionesyan@gmail.com>
2021-03-07 15:05:40 -0300

    feat: add mail config and .env props

--- src/providers/mail-provider/implementations/nodemailer/nodemailer-mail.provider.ts
+++ src/providers/mail-provider/implementations/nodemailer/nodemailer-mail.provider.ts
@@ -1,7 +1,17 @@
+import mailConfig from '@config/mail';
 import SendMailDto from '@providers/mail-provider/contracts/dtos/send-mail.dto';
 import MailProvider from '@providers/mail-provider/contracts/models/mail.provider';
 
 class NodemailerMailProvider implements MailProvider {
+  private transporter = createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    auth: {
+      user: mailConfig.user,
+      pass: mailConfig.pass,
+    },
+  });
+
   send(data: SendMailDto): void {
     throw new Error('Method not implemented.');
   }
