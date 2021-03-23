2a20466f5ce04a2cc2bcc1d2ec7f0f0c95ac64ee
Kow <kaionesyan@gmail.com>
2021-03-23 19:12:44 -0300

    fix: fix isInGroupUniverse function

--- src/shared/utils/is-in-group-universe.ts
+++ src/shared/utils/is-in-group-universe.ts
@@ -1,5 +1,5 @@
 const isInGroupUniverse = (groups: string[], universe: string[]): boolean => {
-  return groups.some(group => !universe.includes(group));
+  return groups.every(group => universe.includes(group));
 };
 
 export default isInGroupUniverse;
