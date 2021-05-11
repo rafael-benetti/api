2bedf1ecf4e9aba7eba1d48489fa6114e82d29e2
kow <kaionesyan@gmail.com>
2021-05-11 17:56:55 -0300

    fix: fix baga

--- src/modules/products/services/remove-from-machine/remove-from-machine.controller.ts
+++ src/modules/products/services/remove-from-machine/remove-from-machine.controller.ts
@@ -6,7 +6,6 @@ import RemoveFromMachineService from './remove-from-machine.service';
 export default abstract class RemoveFromMachineController {
   static validate = celebrate({
     body: {
-      productId: Joi.string().uuid().required(),
       machineId: Joi.string().uuid().required(),
       boxId: Joi.string().uuid().required(),
       quantity: Joi.number().required(),
@@ -16,7 +15,8 @@ export default abstract class RemoveFromMachineController {
 
   static handle: RequestHandler = async (request, response) => {
     const { userId } = request;
-    const { productId, machineId, boxId, quantity, toGroup } = request.body;
+    const { machineId, boxId, quantity, toGroup } = request.body;
+    const { productId } = request.params;
 
     const removeFromMachine = container.resolve(RemoveFromMachineService);
 
