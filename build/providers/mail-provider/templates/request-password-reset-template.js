de09b4047404d6187926b301b8ea91b5380bae6e
davidmacielpi6@gmail.com6 <davidmacielpi6@gmail.com>
2021-06-11 12:46:14 -0300

    fix: a

--- build/providers/mail-provider/templates/request-password-reset-template.js
+++ build/providers/mail-provider/templates/request-password-reset-template.js
@@ -6,8 +6,8 @@ const requestPasswordResetTemplate = (data) => {
         plainText: `${data.receiverName}, você ou outra pessoa solicitou o reset de sua senha Sttigma. Caso tenha sido você, clique no link abaixo para receber uma nova senha neste email. Se não foi você, ignore esse email. Equipe Sttigma.`,
         htmlBody: `
       <h4>${data.receiverName}</h4>,
-      <p>Você ou outra pessoa solicitou o reset de sua senha Sttigma. Caso tenha sido você, clique no link abaixo para receber uma nova senha neste email.
-      <a href="${data.resetPasswordLink}">${data.resetPasswordLink}</a>
+      <p>Você ou outra pessoa solicitou o reset de sua senha Sttigma. Caso tenha sido você, clique no link abaixo para receber uma nova senha neste email.<br/>
+      <a href="${data.resetPasswordLink}">${data.resetPasswordLink}.</a>
       Se não foi você, ignore esse email.</p>
 
       <strong>Equipe Sttigma</strong>
